43f74140964f675c49532b0ff93de568993add9f
Vinay krishna <37305617+vinaykrishnahv@users.noreply.github.com>
2023-10-03 11:41:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// MotorSection JS
+function Bullet(){
+    document.getElementById("MotorImg").src="./images/motorsection/bullet.jpg";
+}
+function Interceptor(){
+    document.getElementById("MotorImg").src="./images/motorsection/Interceptor.avif";
+}
+function Continental(){
+    document.getElementById("MotorImg").src="./images/motorsection/continental.jpg";
+}
+function Supermeteor(){
+    document.getElementById("MotorImg").src="./images/motorsection/supermeteor.avif";
+}
+function Hunter(){
+    document.getElementById("MotorImg").src="./images/motorsection/Hunter.avif";
+}
+function Scram(){
+    document.getElementById("MotorImg").src="./images/motorsection/Scram.avif";
+}
+function Classic(){
+    document.getElementById("MotorImg").src="./images/motorsection/classic.jpg";
+}
+function Meteor(){
+    document.getElementById("MotorImg").src="./images/motorsection/meteor.jpg";
+}
+function Himalayan(){
+    document.getElementById("MotorImg").src="./images/motorsection/Himalayan.jpg";
+}
+//RidesSection JS
+function Motoverse(){
+    document.getElementById("RidesImg").src="./images/ridesection/motoverse.webp";
+}
+function Uncover(){
+    document.getElementById("RidesImg").src="./images/ridesection/Uncover.jpg";
+}
+function Track(){
+    document.getElementById("RidesImg").src="./images/ridesection/Trackschool.avif";
+}
+function ChangThang(){
+    document.getElementById("RidesImg").src="./images/ridesection/changthang.avif";
+}
+function GTCup(){
+    document.getElementById("RidesImg").src="./images/ridesection/GTCup.jpg";
+}
+function Odyssey(){
+    document.getElementById("RidesImg").src="./images/ridesection/odyssey.avif";
+}
+function LehRider(){
+    document.getElementById("RidesImg").src="./images/ridesection/lehrider.jpg";
+}
+function Mapped(){
+    document.getElementById("RidesImg").src="./images/ridesection/Mapped.jpg";
+}
